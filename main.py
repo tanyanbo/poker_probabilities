c71273e26752c9929fe9467985032b50c5583b1e
Tan Yanbo <tanyanbo95@hotmail.com>
2022-08-13 23:39:46 +0800

    Add sampling

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from itertools import combinations
 from Hand import Hand
 from translate import str_to_val
 from compare import compare
+from random import sample
 
-p1 = ['ts', 'th']
-p2 = ['qs', '6h']
+p1 = ['7d', 'jc']
+p2 = ['9s', '8d']
 
 
 def translate():
@@ -14,12 +15,17 @@ def translate():
     return v1, v2, filtered_cards
 
 
-def get_probability():
+def get_probability(should_sample: bool = True, sample_size: int = 10000):
     h1, h2, filtered = translate()
     hand1_count = 0
     hand2_count = 0
     tie_count = 0
-    for community_cards in combinations(filtered, 5):
+    all_combinations = list(combinations(filtered, 5))
+
+    if should_sample:
+        all_combinations = sample(all_combinations, sample_size)
+
+    for community_cards in all_combinations:
         hand1 = Hand(list(community_cards) + h1)
         hand2 = Hand(list(community_cards) + h2)
         res = compare(hand1, hand2)
@@ -31,8 +37,9 @@ def get_probability():
             tie_count += 1
 
     total = hand1_count + hand2_count + tie_count
-    return {'p1': hand1_count / total, 'p2': hand2_count / total,
-            'tie': tie_count / total}
+    return {'p1': f'{round(100 * hand1_count / total, 2)}%',
+            'p2': f'{round(100 * hand2_count / total, 2)}%',
+            'tie': f'{round(100 * tie_count / total, 2)}%'}
 
 
 if __name__ == '__main__':
